a9ba4640792414f943e9f45c384d57ce95184543
Kevin McCabe <kmccabe1@email.unc.edu>
2022-11-09 19:51:07 -0500

    Updated endpoint responses

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ const app = express()
 const args = minimist(process.argv.slice(2))
 
 // Default to port 5000 if no arg given
-let port = 5000
-if (args.port) { port = args.port }
+let port = args.port ? args.port : 5000
 // Default values for dice roll
 const sides = 6
 const dice = 2
@@ -27,37 +26,28 @@ app.get('/app/', (req, res, next) => {
 // Create endpoint for /app/roll/ that returns JSON for a default roll
 app.get('/app/roll/', (req, res, next) => {
 	// Check if args were provided as JSON or URLencoded
-	let arg_sides = sides
-  if (req.body.sides) {
-	  arg_sides = parseInt(req.body.sides)
-  }
-	let arg_dice = dice
-  if (req.body.dice) { 
-	  arg_dice = parseInt(req.body.dice)
-  }
-	let arg_rolls = rolls
-  if (req.body.rolls) { 
-	  arg_rolls = parseInt(req.body.rolls)
-  }
-	res.status(200).type('json').json(roll(arg_sides, arg_dice, arg_rolls))
+	const arg_sides = req.body.sides ? req.body.sides : sides
+	const arg_dice = req.body.dice ? req.body.dice : dice
+	const arg_rolls = req.body.rolls ? req.body.rolls : rolls
+	res.status(200).json(roll(arg_sides, arg_dice, arg_rolls))
 })
 
 // Dice roll endpoint for /app/roll/:sides/
 app.get('/app/roll/:sides/', (req, res, next) => {
 	// Roll dice with sides param
-	res.status(200).type('json').json(roll(parseInt(req.params.sides), dice, rolls))	
+	res.status(200).json(roll(parseInt(req.params.sides), dice, rolls))	
 })
 
 // Dice roll endpoint for /app/roll/:sides/:dice/
 app.get('/app/roll/:sides/:dice/', (req, res, next) => {
 	// Roll dice with sides and dice params
-	res.status(200).type('json').json(roll(parseInt(req.params.sides), parseInt(req.params.dice), rolls))
+	res.status(200).json(roll(parseInt(req.params.sides), parseInt(req.params.dice), rolls))
 })
 
 // Dice roll endpoint for /app/roll/:sides/:dice/:rolls/
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
 	// Roll dice with sides, dice, and rolls params
-	res.status(200).type('json').json(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)))
+	res.status(200).json(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)))
 })
 
 // Default endpoint for undefined endpoints
